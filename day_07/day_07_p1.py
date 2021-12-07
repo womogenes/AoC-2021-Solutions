5e5e22df555097afad02ce074704a1af998b06cd
William Y. Feng <williamfeng1729@gmail.com>
2021-12-06 21:12:56 -0800

    Fix misinterpretation for part 1

--- day_07/day_07_p1.py
+++ day_07/day_07_p1.py
@@ -5,7 +5,9 @@ data = [int(i) for i in raw_data]
 
 ans = 1 << 60
 
-for pos in data:
+max_pos = max(data)
+
+for pos in range(max_pos):
     req = 0
     for i in data:
         req += abs(pos - req)
